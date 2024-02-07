60f6a3eb569ee08d6009e7cc0b2eb864d8506e54
Lucas Lima <lucas@lucas.com.br>
2024-02-07 13:49:15 -0300

    fix isValid equals true when exception was thrown

--- src/main/java/jwt/infrastructure/exception/ExceptionHandler.java
+++ src/main/java/jwt/infrastructure/exception/ExceptionHandler.java
@@ -24,7 +24,7 @@ public class ExceptionHandler implements io.micronaut.http.server.exceptions.Exc
 
         monitor.log(logData);
 
-        return HttpResponse.ok(new JwtValidationResponse(true));
+        return HttpResponse.ok(new JwtValidationResponse(false));
     }
 
     private LogData getLogData(JwtValidationException exception) {
